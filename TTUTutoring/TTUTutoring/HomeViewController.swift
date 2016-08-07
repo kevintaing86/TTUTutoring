da3a96adea54dfbb96eca6cdddc7dfa4d231a74e
Kevin Taing <kevintaing@Kevins-MBP-2.attlocal.net>
2016-08-06 21:48:43 -0500

    Created the project and added a navigation bar

---
+++ TTUTutoring/TTUTutoring/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  TTUTutoring
+//
+//  Created by Kevin Taing on 8/6/16.
+//  Copyright © 2016 Kevin Taing. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Outlets and members
+    @IBOutlet weak var homeVavigationItem: UINavigationItem!
+    
+    // MARK: - Actions and methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ TTUTutoring/TTUTutoring/MainNavigationController.swift
@@ -0,0 +1,26 @@
+//
+//  MainNavigationController.swift
+//  TTUTutoring
+//
+//  Created by Kevin Taing on 8/6/16.
+//  Copyright © 2016 Kevin Taing. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationBar.backgroundColor = UIColor(red:204.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        self.navigationBar.barTintColor = UIColor(red:204.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
