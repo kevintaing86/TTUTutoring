bda5901cc72eaa77357344df08da53f6d54407d5
Kevin Taing <kevintaing86@gmail.com>
2016-08-08 22:12:50 -0500

    Finished populating static data

--- TTUTutoring/DataSource.swift
+++ TTUTutoring/DataSource.swift
@@ -47,5 +47,65 @@ class TutorData {
         Bobbie.tutorSubjects = ["Math", "Physics"]
         Bobbie.tutorSchedule = ["Monday-Friday":"9-11am, 2:30-4:30pm"]
         Tutors?.append(Bobbie)
+        
+        Chris.tutorName = "Chris"
+        Chris.tutorSubjects = ["Computer Sci.", "Math", "Physics"]
+        Chris.tutorSchedule = ["Monday-Friday":"1-5pm"]
+        Tutors?.append(Chris)
+        
+        Cole.tutorName = "Cole"
+        Cole.tutorSubjects = ["Chemistry", "Math"]
+        Cole.tutorSchedule = ["Monday-Friday":"8am-12pm"]
+        Tutors?.append(Cole)
+        
+        Daniel.tutorName = "Daniel"
+        Daniel.tutorSubjects = ["Accounting", "Finance", "Math"]
+        Daniel.tutorSchedule = ["Monday-Friday":"1-5pm"]
+        Tutors?.append(Daniel)
+        
+        David.tutorName = "David"
+        David.tutorSubjects = ["Math", "Mech. Eng.", "Physics"]
+        David.tutorSchedule = ["Monday-Friday":"1-5pm"]
+        Tutors?.append(David)
+        
+        Dhruuv.tutorName = "Dhruuv"
+        Dhruuv.tutorSubjects = [""]
+        Dhruuv.tutorSchedule = ["Monday-Friday":"10am-2pm"]
+        Tutors?.append(Dhruuv)
+        
+        Elaina.tutorName = "Elaina"
+        Elaina.tutorSubjects = ["Math", "Statistics"]
+        Elaina.tutorSchedule = ["Monday":"9:45-11:15pm", "Wednesday":"9:45-11:15pm"]
+        Tutors?.append(Elaina)
+        
+        Kovi.tutorName = "Kovi"
+        Kovi.tutorSubjects = ["Economics", "Finance"]
+        Kovi.tutorSchedule = ["Monday-Friday":"2-5pm"]
+        Tutors?.append(Kovi)
+        
+        Levin.tutorName = "Levin"
+        Levin.tutorSubjects = ["Math", "Physics"]
+        Levin.tutorSchedule = ["Monday-Thursday":"2-5pm", "Friday":"10am-2pm"]
+        Tutors?.append(Levin)
+        
+        Megan.tutorName = "Megan"
+        Megan.tutorSubjects = ["English", "Statistics", "Pol. Sci/Pysch"]
+        Megan.tutorSchedule = ["Monday-Friday":"11am-3pm"]
+        Tutors?.append(Megan)
+        
+        Solabomi.tutorName = "Solabomi"
+        Solabomi.tutorSubjects = ["Economics", "Pol. Sci/Pysch"]
+        Solabomi.tutorSchedule = ["Monday-Friday":"8-11:45am"]
+        Tutors?.append(Solabomi)
+        
+        Yewande.tutorName = "Yewande"
+        Yewande.tutorSubjects = ["Accounting"]
+        Yewande.tutorSchedule = ["Monday-Thursday":"9am-2pm"]
+        Tutors?.append(Yewande)
     }
 }
+
+
+
+
+
